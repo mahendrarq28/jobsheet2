b4b441c0925774d196dafbe0d326e7f915e0e204
Your Name <hendrarq28gmail.com>
2026-09-10 20:53:26 +0700

    latihan_1.2_lagi

--- bagian1.js
+++ bagian1.js
@@ -1,11 +1,26 @@
 function calculateDiscountedPrice(price, discountPercent) {
   return price - (price * discountPercent) / 100;
 }
+const cart = [
+  { title: "Laptop", price: 1000, discountPercent: 10 },
+  { title: "Mouse", price: 20, discountPercent: 5 },
+  { title: "Keyboard", price: 50, discountPercent: 0 }
+];
+
 function applyDiscounts(cart) {
   const result = [];
+
   for (const item of cart) {
-    const finalPrice = calculateDiscountedPrice(item.price, item.discountPercent);
-    result.push({ ...item, finalPrice });
+    const finalPrice =
+      item.price - (item.price * item.discountPercent) / 100;
+
+    result.push({
+      ...item,
+      finalPrice: finalPrice
+    });
   }
+
   return result;
 }
+
+console.log(applyDiscounts(cart));
