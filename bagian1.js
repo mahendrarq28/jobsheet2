613ea49c291eaf2801ffce86aa8b946f74b46191
Your Name <hendrarq28gmail.com>
2026-09-10 20:43:31 +0700

    latihan_1.2

--- bagian1.js
+++ bagian1.js
@@ -1,3 +1,11 @@
 function calculateDiscountedPrice(price, discountPercent) {
   return price - (price * discountPercent) / 100;
 }
+function applyDiscounts(cart) {
+  const result = [];
+  for (const item of cart) {
+    const finalPrice = calculateDiscountedPrice(item.price, item.discountPercent);
+    result.push({ ...item, finalPrice });
+  }
+  return result;
+}
